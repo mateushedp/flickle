f018a7e9c2091f3eeddd0dbe665c38da91d1b63f
mateushedp <mateushedlundp@gmail.com>
2026-03-09 10:12:06 -0300

    refactor: migrate Poster to TypeScript

--- src/components/ui/poster.tsx
+++ src/components/ui/poster.tsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import Image from "next/image";
+import { Movie } from "@/types";
 import { cn } from "./../../lib/utils";
 
+interface PosterProps {
+	movie: Movie;
+}
 
-function Poster({movie}) {
+function Poster({movie}: PosterProps) {
 	const [isLoading, setIsLoading] = useState(true);
 
 	return (
 		<div className="relative w-full h-full">
-			{isLoading && 
-						<div className="absolute inset-0 bg-neutral-300 animate-shimmer " />}
+			{isLoading && (
+				<div className="absolute inset-0 bg-neutral-300 animate-shimmer " />)}
 			<Image 
 				src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`} 
-				alt="Movie poster"
+				alt={movie.title || "Movie poster"}
 				fill
 				className={cn(
 					"background-cover object-cover transition-opacity duration-500",
