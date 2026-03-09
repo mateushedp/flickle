95ed09597d23388f62eb940de2c79e523d9aa6a6
mateushedp <mateushedlundp@gmail.com>
2026-03-09 10:58:39 -0300

    refactor: move title to _app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,10 @@
-/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from "next/document";
 import type { DocumentProps } from "next/document";
 
 export default function Document({}: DocumentProps) {
 	return (
 		<Html lang="en">
-			 <Head>
-				<title>Flickle</title>
-          	</Head>
+			<Head />
 			<body className="antialiased">
 				<Main />
 				<NextScript />
