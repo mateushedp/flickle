fe2aac01f771d13c65506a1b4a8997785114d718
mateushedp <mateushedlundp@gmail.com>
2026-03-11 09:26:42 -0300

    fix: add null safety to formatMovies

--- src/lib/format-movies.ts
+++ src/lib/format-movies.ts
@@ -1,15 +1,14 @@
 import { Movie, RawMovie } from "@/types";
 
-
 export function formatMovies(rawMovieArray: RawMovie[]): Omit<Movie, "id" | "createdAt" | "updatedAt" | "dailyMovies">[] {
 	return rawMovieArray.map(rawMovie => ({
-		title: rawMovie.title,
-		poster_path: rawMovie.poster_path,
-		genre: rawMovie.genres[0].name,
-		release_year: parseInt(rawMovie.release_date.substring(0, 4)),
-		country: rawMovie.origin_country[0] ?? "",
-		runtime: rawMovie.runtime,
-		score: Math.round(parseFloat(rawMovie.vote_average) * 100) / 100,
-		budget: rawMovie.budget,
+		title: rawMovie.title ?? "Unknown",
+		poster_path: rawMovie.poster_path ?? "",
+		genre: rawMovie.genres?.[0]?.name ?? "Unknown",
+		release_year: parseInt(rawMovie.release_date?.substring(0, 4) ?? "0"),
+		country: rawMovie.origin_country?.[0] ?? "",
+		runtime: rawMovie.runtime ?? 0,
+		score: Math.round(parseFloat(rawMovie.vote_average ?? "0") * 100) / 100,
+		budget: rawMovie.budget ?? 0,
 	}));
 }
