d6ab7adf5dccec215f27b65766851251105e07c8
mateushedp <mateushedlundp@gmail.com>
2026-03-11 09:52:16 -0300

    refactor: add showTitle prop to Poster component

--- src/components/ui/poster.tsx
+++ src/components/ui/poster.tsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 import { Movie } from "@/types";
-import { cn } from "./../../lib/utils";
 
 interface PosterProps {
 	movie: Movie;
+	showTitle?: boolean;
 }
 
-function Poster({movie}: PosterProps) {
+function Poster({ movie, showTitle = false }: PosterProps) {
 	const [isLoading, setIsLoading] = useState(true);
 
 	return (
 		<div className="relative w-full h-full">
 			{isLoading && (
-				<div className="absolute inset-0 bg-neutral-300 animate-shimmer " />)}
-			<Image 
-				src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`} 
+				<div className="absolute inset-0 bg-neutral-300 animate-shimmer" />
+			)}
+			<Image
+				src={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
 				alt={movie.title || "Movie poster"}
 				fill
 				className={cn(
@@ -24,6 +26,11 @@ function Poster({movie}: PosterProps) {
 				)}
 				onLoadingComplete={() => setIsLoading(false)}
 			/>
+			{showTitle && (
+				<div className="w-full min-h-[30px] max-h-[60px] absolute bottom-0 border bg-white flex items-center justify-center">
+					<p className="font-bold text-[12px] text-center line-clamp-3">{movie.title}</p>
+				</div>
+			)}
 		</div>
 	);
 }
